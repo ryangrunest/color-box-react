a6e12d0d5d3a04b3b587389810ec333dafcfe11e
Ryan Grunest <ryangrunest45@gmail.com>
2019-07-23 13:23:39 -0700

    background colors change onclick

--- src/ColorBox/index.js
+++ src/ColorBox/index.js
@@ -5,15 +5,24 @@ class ColorBox extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      
+      colorArr: ['#097054', '#FFDE00', '#6599FF', '#FF9900'],
+      background: '#012FJ9'
     }
   }
-  static defaultProps = {
 
+  pickRandomColor() {
+    // alert('clicked!');
+    let pickedColor = this.state.colorArr[Math.floor(Math.random() * 4)];
+    console.log(pickedColor);
+    this.setState({
+      background: pickedColor
+    });
+  }
+  static defaultProps = {
   }
   render() {
     return(
-      <div className="ColorBox"></div>
+      <div style={{backgroundColor: this.state.background}} onClick={() => this.pickRandomColor()} className="ColorBox"></div>
     )
   }
 }
